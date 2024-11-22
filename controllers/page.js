5f17739b828626d41138f4c791891ec5a74f72da
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-11-22 21:46:31 +0400

    fucking wokerrr

--- controllers/page.js
+++ controllers/page.js
@@ -1,37 +1,37 @@
 function getIndex(req, res) {
-    res.status(200).render('index');
+    res.status(200).render('worker/index');
 };
 
 function getAdminLogin(req, res) {
-    res.status(200).render('partials/admin/login');
+    res.status(200).render('admin/login');
 };
 
 function getAdmin(req, res) {
-    res.status(200).render('partials/admin/index', {
+    res.status(200).render('admin/index', {
         activePage: 'home'
     });
 };
 
 function getTable(req, res) {
-    res.status(200).render('partials/admin/index', {
+    res.status(200).render('admin/index', {
         activePage: 'table'
     });
 };
 
 function getWarehouse(req, res) {
-    res.status(200).render('partials/admin/index', {
+    res.status(200).render('admin/index', {
         activePage: 'warehouse'
     });
 };
 
 function getWithoutWarehouse(req, res) {
-    res.status(200).render('partials/admin/index', {
+    res.status(200).render('admin/index', {
         activePage: 'without-warehouse'
     });
 };
 
 function getExpense(req, res) {
-    res.status(200).render('partials/admin/index', {
+    res.status(200).render('admin/index', {
         activePage: 'expense'
     });
 };

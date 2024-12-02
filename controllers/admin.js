4438feb73c41032137a21f0b21e74944d52bc44f
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-12-03 03:22:44 +0400

    jhbsaudihsauida

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,6 +11,9 @@ import {
 function login(req, res) {
     try {
         const { password } = req.body;
+        console.log(req.body);
+
+        console.log(process.env.PASSWORD);
 
         if (!password) {
             return res.status(400).json({
